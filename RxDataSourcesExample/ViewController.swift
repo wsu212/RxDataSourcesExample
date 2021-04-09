b286e2797275158cacf06e60e11be9f8baf7652c
Wei-Lun Su <allensu212@gmail.com>
2021-04-09 19:51:02 -0400

    initial commit

--- RxDataSourcesExample/ViewController.swift
+++ RxDataSourcesExample/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupSubviews()
     }
-
-
+    
+    private func setupSubviews() {
+        view.backgroundColor = .white
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
 }
 
